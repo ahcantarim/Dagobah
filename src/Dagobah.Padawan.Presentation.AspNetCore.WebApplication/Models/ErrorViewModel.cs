e4ff0b7efb6d5265ab84c732ce9f476741d8a4b8
ahcantarim <andre.cantarim@gmail.com>
2021-03-23 17:37:43 -0300

    Add Presentation AspNetCore WebApplication

---
+++ src/Dagobah.Padawan.Presentation.AspNetCore.WebApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Dagobah.Padawan.Presentation.AspNetCore.WebApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ src/Dagobah.Padawan.Presentation.AspNetCore.WebApplication/ViewModels/UsuarioViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Dagobah.Padawan.Presentation.AspNetCore.WebApplication.ViewModels
+{
+    public class UsuarioViewModel
+    {
+        [Key]
+        public int Id { get; private set; }
+
+        [Required(ErrorMessage = "Preencha o e-mail.")]
+        [MaxLength(150, ErrorMessage = "Comprimento máximo de {0} caracteres.")]
+        [MinLength(5, ErrorMessage = "Comprimento mínimo de {0} caracteres.")]
+        [EmailAddress(ErrorMessage = "Preencha com um e-mail válido.")]
+        [DisplayName("E-mail")]
+        public string Email { get; private set; }
+
+        [ScaffoldColumn(false)]
+        public string Senha { get; private set; }
+
+        [ScaffoldColumn(false)]
+        public bool Confirmado { get; private set; }
+
+        [ScaffoldColumn(false)]
+        public DateTime? UltimoAcessoEm { get; private set; }
+    }
+}
